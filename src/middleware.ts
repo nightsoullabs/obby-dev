77217e00c6334583048c17b25dddf65370882145
Sree Narayanan <sreeaadhi07@gmail.com>
2025-06-08 13:32:47 +0400

    bruh

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,4 +7,18 @@ export default authkitMiddleware();
 
 // Match against pages that require authentication
 // Leave this out if you want authentication on every page in your application
-export const config = { matcher: ["/"] };
+export const config = {
+  matcher: [
+    "/",
+    "/pricing",
+    "/chat/:path*",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
